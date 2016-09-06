30d4af220bd55ca778b82055e016553890ea1620
Thomas Månsson <thomasmonsson@gmail.com>
2016-09-06 14:51:53 +0200

    Start project and add some assets

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -7,6 +7,38 @@
 //
 
 import UIKit
+import SceneKit
+class GameViewController: UIViewController {
+    
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+    }
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    func setupView() {
+        scnView = self.view as! SCNView
+    }
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+    }
+}
+
+
+
+
+
+
+/*import UIKit
 import QuartzCore
 import SceneKit
 
@@ -122,4 +154,4 @@ class GameViewController: UIViewController {
         // Release any cached data, images, etc that aren't in use.
     }
 
-}
+}*/
